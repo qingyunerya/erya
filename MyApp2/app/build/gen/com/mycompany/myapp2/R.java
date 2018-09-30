9c953734e81022d3b6bd7e843437420b7df263b5
qingyunerya <2629615164@qq.com>
2018-09-30 18:35:23 +0800

    meizi

--- MyApp2/app/build/gen/com/mycompany/myapp2/R.java
+++ MyApp2/app/build/gen/com/mycompany/myapp2/R.java
@@ -12,9 +12,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int image_1=0x7f020001;
+        public static final int image_2=0x7f020002;
+    }
+    public static final class id {
+        public static final int meiziFrameLayout1=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int meizi=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- MyApp2/app/src/main/java/com/mycompany/myapp2/MainActivity.java
+++ MyApp2/app/src/main/java/com/mycompany/myapp2/MainActivity.java
@@ -1,14 +1,32 @@
 package com.mycompany.myapp2;
+import android.os.Bundle;  
+import android.view.MotionEvent;  
+import android.view.View;  
+import android.view.View.OnTouchListener;  
+import android.widget.FrameLayout;  
+import android.app.Activity;  
 
-import android.app.*;
-import android.os.*;
 
-public class MainActivity extends Activity 
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity {  
+
+    @Override  
+    protected void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.meizi);  
+        FrameLayout frame = (FrameLayout) findViewById(R.id.meiziFrameLayout1);  
+        final MeiziView mezi = new MeiziView(MainActivity.this);  
+        //为我们的萌妹子添加触摸事件监听器  
+        mezi.setOnTouchListener(new OnTouchListener() {  
+				@Override  
+				public boolean onTouch(View view, MotionEvent event) {  
+					//设置妹子显示的位置  
+					mezi.bitmapX = event.getX() - 150;  
+					mezi.bitmapY = event.getY() - 150;  
+					//调用重绘方法  
+					mezi.invalidate();  
+					return true;  
+				}  
+			});  
+        frame.addView(mezi);  
+    }  
 }
